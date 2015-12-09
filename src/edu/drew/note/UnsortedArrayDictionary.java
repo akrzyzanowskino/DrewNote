ad973c8e6013e41e17d65c244e486daebe34e216
Alexandra Krzyzanowski Novitz <akrzyzanowskino@drew.edu>
2015-12-09 15:48:57 -0500

    //debugging, fixes

--- src/edu/drew/note/UnsortedArrayDictionary.java
+++ src/edu/drew/note/UnsortedArrayDictionary.java
@@ -1,11 +1,13 @@
 package edu.drew.note;
+
+import java.util.Arrays;
+
 //Alexandra
 //dictionary implemented with an unsorted array
 public class UnsortedArrayDictionary implements NoteCollection {
 	private int numEntries = 0;
 	private Note [] arrayDictionary;
-	private boolean initialized = false;
-	private final static int DEFAULT_CAPACITY = 25;
+	private final static int DEFAULT_CAPACITY = 100;
 	private static final int MAX_CAPACITY = 100;
 	
 	
@@ -13,12 +15,18 @@ public class UnsortedArrayDictionary implements NoteCollection {
 		arrayDictionary = (Note[]) new Note[DEFAULT_CAPACITY];
 	}
 	
-
+	private boolean isArrayFull() {
+		return numEntries >= arrayDictionary.length;
+	}
+	
 	@Override
 	public boolean add(Note newNote) {
+		if (!isArrayFull()) {
 		arrayDictionary[numEntries] = newNote;
 	      numEntries++;
 	      return true;
+		}
+		return false;
 	}
 	
 
@@ -30,13 +38,16 @@ public class UnsortedArrayDictionary implements NoteCollection {
 		
 		return null;
 	}
+	
+	public  void getID(long ID){
+		return;
+	}
 
 	@Override
 	public boolean remove(long ID) {
 		for (int i = 0; i < numEntries; i++) {
-			
+	
 			if (arrayDictionary[i].equals(ID)) {
-				
 				arrayDictionary[i] = arrayDictionary[numEntries-1];
 				numEntries--;
 				
@@ -48,19 +59,17 @@ public class UnsortedArrayDictionary implements NoteCollection {
 
 	@Override
 	public boolean remove(Note note) {
-	
 		for (int i = 0; i < numEntries; i++) {
 			
 			if (arrayDictionary[i].equals(note)) {
-			
 				arrayDictionary[i] = arrayDictionary[numEntries-1];
 				numEntries--;
-				
 				return true;
 			}
 		}
 		return false;
 	}
+	
 	
 
 	@Override
@@ -75,19 +84,29 @@ public class UnsortedArrayDictionary implements NoteCollection {
 	public int getSize() {
 		return numEntries;
 	}
+	
+	public int clear(){
+		numEntries=0;
+		return numEntries;
+	}
 
 	@Override
 	public boolean contains(Note note) {
-		for (Note n: arrayDictionary){
-		if (n.equals(note))
-			return true;
-		}
-			return false;
+		return Arrays.asList(arrayDictionary).contains(note);
+//		for( Note n : arrayDictionary){
+//			if(n.equals(note))
+//				return true;
+//			else if(n.equals(null)){
+//				return true;
+//			}
+//		}
+//		return false;
 	}
 
 	@Override
 	public boolean contains(long ID) {
-		return lookup(ID) != null;			
+		return lookup(ID) != null;		
+		//return Arrays.asList(arrayDictionary).contains(ID);
 	}
 
 	@Override
